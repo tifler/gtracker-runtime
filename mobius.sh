5133c8943b2ee0be6b83d822feaab0d55ac6c28c
Youngdo, Lee <ydlee@anapass.com>
2018-12-28 10:01:25 +0900

    Make mobius run in background

--- mobius.sh
+++ mobius.sh
@@ -9,7 +9,21 @@ case "$1" in
     start)
         echo -n "Starting docker: "
         docker \
-            run -it \
+            run \
+            --rm \
+            --name ${DOCKERNAME} \
+            --env MOBIUS_DB_PASS=${MARIA_DB_PASS} \
+            --env MOBIUS_DB_PORT=${MARIA_DB_PORT} \
+            --env MOBIUS_DB_HOST=${MARIA_DB_HOST} \
+            --publish ${MOBIUS_HTTP_PORT}:7579 \
+            --publish ${MOBIUS_MQTT_PORT}:1883 \
+            ${DOCKER} bash /bin/start-mobius.sh
+    ;;
+    start-it)
+        echo -n "Starting docker: "
+        docker \
+            run \
+            -it \
             --rm \
             --name ${DOCKERNAME} \
             --env MOBIUS_DB_PASS=${MARIA_DB_PASS} \
