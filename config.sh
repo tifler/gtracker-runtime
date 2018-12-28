6d7f73e5ef60a9dfd1523a951f85db80c8ee6ba5
Youngdo, Lee <ydlee@anapass.com>
2018-12-28 12:41:39 +0900

    Remove unused variables

--- config.sh
+++ config.sh
@@ -14,10 +14,8 @@ export MOBIUS_HOST=${MARIA_DB_HOST}
 export MOBIUS_HTTP_PORT=7579
 export MOBIUS_MQTT_PORT=1883
 export MOBIUS_NOTI_PORT=9727
-export MOBIUS_BASE=/home/tifler/work/iot/server/mobius-src/
 
 # GTracker
-export GTRACKER_BASE=/home/tifler/work/iot/server/gtracker-server/anapass/
 export GTRACKER_SERVER_PORT=8080
 export GTRACKER_DB_USER=${MARIA_DB_USER}
 export GTRACKER_DB_PASS=${MARIA_DB_PASS}
