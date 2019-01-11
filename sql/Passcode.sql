797e5da5b7254e5d0eeaef552bbc3bc28751b627
Youngdo, Lee <ydlee@anapass.com>
2019-01-11 17:29:17 +0900

    Add Settings to GTrackerAdmin DB

--- sql/Passcode.sql
+++ sql/Passcode.sql
@@ -1,4 +1,5 @@
 CREATE TABLE Passcode(
-PSID INT(11) auto_increment PRIMARY KEY,
-Passcode NVARCHAR(20) NOT NULL,
-SerialNo NVARCHAR(20) NOT NULL);
+    PSID INT(11) auto_increment PRIMARY KEY,
+    SerialNo VARCHAR(32) UNIQUE NOT NULL,
+    Passcode VARCHAR(32) NOT NULL
+);
